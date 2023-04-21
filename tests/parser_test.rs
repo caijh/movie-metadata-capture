14c06c600fa5c9459367758f6da7550f311f891d
蔡俊辉(10039330) <caijh@glodon.com>
2023-04-21 17:28:03 +0800

    Add test for searching a movie using a parser from a config file

--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -3,5 +3,11 @@ mod tests {
     use movie_metadata_capture::config::{get_app_config, load_config_file};
 
     #[tokio::test]
-    async fn test_read_html_as_document() {}
+    async fn test_search_movie() {
+        load_config_file("./Config.toml").await.expect("");
+        let config = get_app_config();
+        let parser = config.sources.get("fanza").unwrap();
+        let movie = parser.search("123").await;
+        assert!(movie.is_some());
+    }
 }
