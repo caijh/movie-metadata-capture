bc589a7615ada0e77c19230fa9566894292de304
johnhuitsai <caiqizhe@gmail.com>
2023-05-10 22:02:47 +0800

    Update Missav parser with new XPath expressions and refactor main.rs to use subcommands for info and scraping

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use chrono::Local;
-use clap::{arg, Parser};
+use clap::{arg, Parser, Subcommand};
 use movie_metadata_capture::config::AppConfig;
 use movie_metadata_capture::core::{scraping_data_and_move_movie, scraping_data_and_move_movie_with_custom_number, movie_lists};
 use movie_metadata_capture::number_parser::{get_number, DEFAULT_NUMBER_EXTRACTOR};
@@ -9,10 +9,11 @@ use std::ops::Not;
 use std::path::Path;
 use std::time::Duration;
 use std::{thread, time};
+use movie_metadata_capture::scraping::Scraping;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let args: Args = Args::parse();
+    let args: Cli = Cli::parse();
 
     let config_path = args.config_path.unwrap_or("./config.toml".to_string());
     println!("[+]Load AppConfig from config file '{}'.", config_path);
@@ -26,50 +27,64 @@ async fn main() -> Result<(), Box<dyn Error>> {
         return Err("[-] Main mode must be 1 or 2 or 3!".into());
     }
 
-    config.create_failed_folder().await?;
-
     let start_time = time::Instant::now();
     println!(
         "[+]Start at {}",
         Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
     );
 
-    let single_file_path = args.single_file_path.unwrap_or_default();
-    if !single_file_path.is_empty() {
-        println!("[+]==================== Single File =====================");
-        let (custom_number, number_extractor) = if args.custom_number.is_none() {
-            get_number(&config, single_file_path.as_str()).unwrap()
-        } else {
-            (args.custom_number.unwrap_or_default(), DEFAULT_NUMBER_EXTRACTOR.to_owned())
-        };
-        scraping_data_and_move_movie_with_custom_number(
-            single_file_path.as_str(),
-            custom_number.as_str(),
-            &number_extractor,
-            args.specified_source,
-            &config,
-        )
-        .await?;
-    } else {
-        let folder_path = if config.common.source_folder.is_empty().not() {
-            Path::new(&config.common.source_folder)
-        } else {
-            Path::new(".")
-        };
-        let movie_list = movie_lists(&config, folder_path);
+    match args.subcommand {
+        SubCommand::Info(info_args) => {
+            let  (number, number_extractor)= get_number(&config, info_args.file.as_str()).unwrap();
+            println!("[!][{}] As Number Processing for '{}'", number, info_args.file);
+            let mut scraping = Scraping::new(&config);
+            let movie = scraping
+                .search(&number, &number_extractor, None, Some(info_args.source))
+                .await;
+            if movie.is_some() {
+                println!("{:?}", movie);
+            }
+        },
+        SubCommand::Scraping(scraping_args) => {
+            config.create_failed_folder().await?;
 
-        let movie_count = movie_list.len();
-        println!("[+]Find {} movies.", movie_count);
-        println!("[*]======================================================");
-        for movie_path in movie_list {
-            scraping_data_and_move_movie(movie_path.as_str(), &config).await?;
-            let mut rng = rand::thread_rng();
-            let sleep_seconds = rng.gen_range(config.common.sleep..config.common.sleep + 2);
-            thread::sleep(Duration::from_secs(sleep_seconds));
+            let single_file_path = scraping_args.file.unwrap_or_default();
+            if !single_file_path.is_empty() {
+                println!("[+]==================== Single File =====================");
+                let (custom_number, number_extractor) = if scraping_args.custom_number.is_none() {
+                    get_number(&config, single_file_path.as_str()).unwrap()
+                } else {
+                    (scraping_args.custom_number.unwrap_or_default(), DEFAULT_NUMBER_EXTRACTOR.to_owned())
+                };
+                scraping_data_and_move_movie_with_custom_number(
+                    &single_file_path,
+                    &custom_number,
+                    &number_extractor,
+                    scraping_args.source,
+                    &config,
+                ).await?;
+            } else {
+                let folder_path = if config.common.source_folder.is_empty().not() {
+                    Path::new(&config.common.source_folder)
+                } else {
+                    Path::new(".")
+                };
+                let movie_list = movie_lists(&config, folder_path);
+
+                let movie_count = movie_list.len();
+                println!("[+]Find {} movies.", movie_count);
+                println!("[*]======================================================");
+                for movie_path in movie_list {
+                    scraping_data_and_move_movie(movie_path.as_str(), &config).await?;
+                    let mut rng = rand::thread_rng();
+                    let sleep_seconds = rng.gen_range(config.common.sleep..config.common.sleep + 2);
+                    thread::sleep(Duration::from_secs(sleep_seconds));
+                }
+            }
+
+            config.delete_empty_folder().await?;
         }
     }
-
-    config.delete_empty_folder().await?;
 
     let end_time = time::Instant::now();
     let total_time = end_time.duration_since(start_time);
@@ -80,8 +95,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
     );
 
-    println!("[+]All finished!!!");
-
     if config.common.auto_exit {
         std::process::exit(0);
     }
@@ -91,16 +104,35 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-pub struct Args {
+pub struct Cli {
     #[arg(long, required = false)]
     pub config_path: Option<String>,
 
+    #[command(subcommand)]
+    subcommand: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum SubCommand {
+    Info(InfoArgs),
+    Scraping(ScrapingArgs),
+}
+
+#[derive(Parser, Debug)]
+pub struct InfoArgs {
+    #[arg(long)]
+    pub file: String,
+    #[arg(long)]
+    pub source: String,
+}
+#[derive(Parser, Debug)]
+pub struct ScrapingArgs {
     #[arg(long, required = false)]
-    pub single_file_path: Option<String>,
+    pub file: Option<String>,
 
     #[arg(long, required = false)]
     pub custom_number: Option<String>,
 
     #[arg(long, required = false)]
-    pub specified_source: Option<String>,
+    pub source: Option<String>,
 }
