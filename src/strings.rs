d77471af37cded87601081d38e66f3012eccb09c
johnhuitsai <caiqizhe@gmail.com>
2023-05-11 21:40:30 +0800

    Refactor get_start_index and get_end_index functions to simplify the code and improve readability

--- src/strings.rs
+++ src/strings.rs
@@ -1,19 +1,13 @@
 pub fn get_start_index(s: &str, start: &str) -> usize {
-    let start = match start.parse::<usize>() {
+    match start.parse::<usize>() {
         Ok(number) => number,
-        Err(_) => {
-            s.find(start).unwrap_or_else(|| 0)
-        }
-    };
-    start
+        Err(_) => s.find(start).unwrap_or(0),
+    }
 }
 
 pub fn get_end_index(s: &str, end: &str) -> usize {
-    let end = match end.parse::<usize>() {
+    match end.parse::<usize>() {
         Ok(number) => number,
-        Err(_) => {
-            s.find(end).unwrap_or_else(|| s.len())
-        }
-    };
-    end
+        Err(_) => s.find(end).unwrap_or(s.len()),
+    }
 }
