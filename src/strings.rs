cbbd856c1171b1856d1f8d1a46a247a2dfda959e
johnhuitsai <caiqizhe@gmail.com>
2023-05-11 21:50:45 +0800

    Refactor string functions to handle empty start and end values gracefully

--- src/strings.rs
+++ src/strings.rs
@@ -1,13 +1,25 @@
 pub fn get_start_index(s: &str, start: &str) -> usize {
     match start.parse::<usize>() {
         Ok(number) => number,
-        Err(_) => s.find(start).unwrap_or(0),
+        Err(_) => {
+            if start.is_empty() {
+                0
+            } else {
+                s.find(start).unwrap_or(0)
+            }
+        }
     }
 }
 
 pub fn get_end_index(s: &str, end: &str) -> usize {
     match end.parse::<usize>() {
         Ok(number) => number,
-        Err(_) => s.find(end).unwrap_or(s.len()),
+        Err(_) => {
+            if end.is_empty() {
+                s.len()
+            } else {
+                s.find(end).unwrap_or(s.len())
+            }
+        }
     }
 }
