c3663ce3b40fc86c48ecb43320767e786453713b
johnhuitsai <caiqizhe@gmail.com>
2023-05-13 00:25:30 +0800

    Refactor remove_empty_dirs() to handle non-existent directories and simplify logic for removing empty directories

--- src/files.rs
+++ src/files.rs
@@ -28,18 +28,28 @@ pub async fn rm_empty_folder(dir: &str) -> Result<(), io::Error> {
 }
 // remove_empty_dirs() recursively removes all empty directories in the given directory.
 // Returns an io::Error if any of the text or directory operations fail.
-fn remove_empty_dirs(dir: &str) -> io::Result<()> {
-    for entry in fs::read_dir(dir)? {
+fn remove_empty_dirs(dir_path: &str) -> std::io::Result<()> {
+    let path = Path::new(dir_path);
+    if !path.exists() {
+        return Ok(());
+    }
+
+    // Get a list of all entries in the directory
+    let entries = fs::read_dir(dir_path)?;
+
+    for entry in entries {
         let entry = entry?;
-        let path = entry.path();
-        if path.is_dir() {
-            remove_empty_dirs(&path.display().to_string())?;
-            if let Ok(entries) = fs::read_dir(&path) {
-                if entries.filter_map(|entry| entry.ok()).count() == 0 {
-                    fs::remove_dir(&path)?;
-                }
+
+        // If the entry is a directory, recursively remove empty directories within it
+        if entry.file_type()?.is_dir() {
+            remove_empty_dirs(entry.path().to_str().unwrap())?;
+
+            // Check if the directory is now empty and remove it if it is
+            if fs::read_dir(entry.path())?.next().is_none() {
+                fs::remove_dir(entry.path())?;
             }
         }
     }
+
     Ok(())
 }
